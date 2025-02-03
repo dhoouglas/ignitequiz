5333c76c822f962a2fd6ff2cd7350f0e61ef9b92
dhoouglas <dhoouglas@hotmail.com>
2025-02-03 10:25:52 -0300

    feat: user interaction to delete history

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import "react-native-gesture-handler";
+
 import { StatusBar } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { Routes } from "./src/routes";
 import { Loading } from "./src/components/Loading";
@@ -17,13 +19,13 @@ export default function App() {
   }
 
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <Routes />
-    </>
+    </GestureHandlerRootView>
   );
 }
